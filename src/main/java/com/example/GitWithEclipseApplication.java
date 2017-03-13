76c3dfa48c8119da98e9755d1f3c3dd8bafc3e79
khangnvph04513 <khangnvph04513@gmail.com>
2017-03-13 15:20:47 +0700

    File Khoi tao

---
+++ src/main/java/com/example/GitWithEclipseApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitWithEclipseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitWithEclipseApplication.class, args);
+	}
+}
